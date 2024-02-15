89ecba3e7f21288401a0e2ee9da95ba24dbc9d01
Akito08 <sza9ki8to7@gmail.com>
2024-02-15 14:03:29 +0900

    add action type

--- src/types.ts
+++ src/types.ts
@@ -9,4 +9,6 @@ export type ChallengerStatus =
 
 export type DealerStatus = "Initial" | "Bust" | "Blackjack" | "Hit" | "Stand";
 
+export type Action = "Hit" | "Stand" | "Double";
+
 export type GamePhase = "Betting" | "Acting" | "Evaluating";
